c76e7007544e5b6dfbaaeeb7aa55261b2b4ee13a
GilangPambudi <2131710126@student.polinema.ac.id>
2025-05-28 02:05:12 +0700

    Add SERAPHIM project to portfolio and remove commented-out items

--- lib/portfolio.ts
+++ lib/portfolio.ts
@@ -8,6 +8,14 @@ const portfolioItems = [
     alt: "m111",
   },
   {
+    title: "SERAPHIM",
+    description:
+      "Search About Phone Informations & Models, connected with Phone Specifications API.",
+    image: "/seraphim.png",
+    link: "https://seraphim.pambudi.site/",
+    alt: "m111",
+  },
+  {
     title: "Instagram Mutual Detector",
     description:
       "Find out which Instagram accounts you follow that don't follow you back.",
@@ -30,20 +38,6 @@ const portfolioItems = [
     link: "https://safd-absence-generator.vercel.app/",
     alt: "generator",
   },
-  // {
-  //   title: "Instagram Follow Back Checker",
-  //   description: "Check if someone follows you back on Instagram.",
-  //   image: "/follback.png",
-  //   link: "https://check-follback.vercel.app/",
-  //   alt: "follback",
-  // },
-  // {
-  //   title: "JSON Instagram Converter",
-  //   description: "Easy read followers and following Instagram JSON data.",
-  //   image: "/json.png",
-  //   link: "https://json-ig-converter.vercel.app/",
-  //   alt: "json",
-  // },
   {
     title: "SAFD Absence Finder",
     description:
