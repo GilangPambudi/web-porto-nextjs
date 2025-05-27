cd496b07001b8ab3fad5b540e1e06a28b10f1af2
GilangPambudi <2131710126@student.polinema.ac.id>
2025-05-27 13:41:33 +0700

    Refactor portfolio items into a separate module for better organization

--- components/sections/portfolio-section.tsx
+++ components/sections/portfolio-section.tsx
@@ -1,65 +1,7 @@
 import Image from "next/image"
+import portfolioItems from "@/lib/portfolio" // Pastikan path ini sesuai dengan struktur project Anda
 
 export default function PortfolioSection() {
-    const portfolioItems = [
-        {
-            title: "M111 Reunion Family Event",
-            description: "A website for the M111 event, providing event information and details.",
-            image: "/m111.png",
-            link: "https://ngalamfoldingbike.id/",
-            alt: "m111",
-        },
-        {
-            title: "Instagram Mutual Detector",
-            description: "Find out which Instagram accounts you follow that don't follow you back.",
-            image: "/imud.png",
-            link: "https://imud.vercel.app/",
-            alt: "imud",
-        },
-        {
-            title: "JAMSELINAS 13 2024 MALANG",
-            description: "Information website about the Jambore Sepeda Lipat Nasional event in Malang, built using WordPress.",
-            image: "/jamselinas.png",
-            link: "https://jamselinas13.ngalamfoldingbike.id/",
-            alt: "jamselinas",
-        },
-        {
-            title: "SAFD Absence Generator",
-            description: "Generator Absence BBCode for San Andreas Fire Department.",
-            image: "/generator.png",
-            link: "https://safd-absence-generator.vercel.app/",
-            alt: "generator",
-        },
-        {
-            title: "Instagram Follow Back Checker",
-            description: "Check if someone follows you back on Instagram.",
-            image: "/follback.png",
-            link: "https://check-follback.vercel.app/",
-            alt: "follback",
-        },
-        {
-            title: "JSON Instagram Converter",
-            description: "Easy read followers and following Instagram JSON data.",
-            image: "/json.png",
-            link: "https://json-ig-converter.vercel.app/",
-            alt: "json",
-        },
-        {
-            title: "SAFD Absence Finder",
-            description: "Absence finder by district or name for San Andreas Fire Department",
-            image: "/finder.png",
-            link: "https://safd-absence-finder.vercel.app/",
-            alt: "finder",
-        },
-        {
-            title: "Component Calculator",
-            description: "Calculator component for mechanic Jogjagmers Reality Project.",
-            image: "/component.png",
-            link: "https://jgrp-component-calculator.vercel.app/",
-            alt: "component",
-        },
-    ]
-
     return (
         <section
             id="portfolio"
