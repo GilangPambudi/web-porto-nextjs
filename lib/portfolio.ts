6b6e68886b09e9f484261434ae5105b3a17fa185
GilangPambudi <2131710126@student.polinema.ac.id>
2025-12-23 12:25:52 +0700

    feat: add new portfolio data with new `fnff.webp` and `seraphim.webp` images

--- lib/portfolio.ts
+++ lib/portfolio.ts
@@ -8,12 +8,28 @@ const portfolioItems = [
     alt: "pams",
   },
   {
-    title: "SMADATARARUN 2025",
+    title: "SERAPHIM",
     description:
-      "Official website for the SMADATARARUN 2025 running event, providing registration and event information.",
+      "Search About Phone Informations & Models, connected with phone codename repository.",
+    image: "/seraphim.webp",
+    link: "https://seraphim.pambudi.site/",
+    alt: "seraphim",
+  },
+  {
+    title: "SMADATARARUN 2026",
+    description:
+      "Official website for the SMADATARARUN 2026 running event, providing registration and event information.",
     image: "/smadatararun.webp",
     link: "https://smadatararun.com/",
     alt: "smadatararun",
+  },
+  {
+    title: "Folding Ngalam Freedom for All",
+    description:
+      "A landing page of Folding Ngalam Freedom for All event, providing event information and details.",
+    image: "/fnff.webp",
+    link: "https://freedom.ecofest.id/",
+    alt: "freedom",
   },
   {
     title: "M111 Reunion Family Event",
@@ -24,27 +40,11 @@ const portfolioItems = [
     alt: "m111",
   },
   {
-    title: "JGRP Vehicle Price Calculator",
-    description:
-      "Calculate dealer price, insurance, copayment, and '/sellveh' price, built with Next.js. ",
-    image: "/jg-dealer.png",
-    link: "https://jg-veh.vercel.app/",
-    alt: "jg-veh",
-  },
-  {
-    title: "SERAPHIM",
-    description:
-      "Search About Phone Informations & Models, connected with phone codename repository.",
-    image: "/seraphim.png",
-    link: "https://seraphim.pambudi.site/",
-    alt: "seraphim",
-  },
-  {
     title: "Instagram Mutual Detector",
     description:
       "Find out which Instagram accounts you follow that don't follow you back.",
     image: "/imud.png",
-    link: "https://imud.vercel.app/",
+    link: "https://imud.pambudi.site/",
     alt: "imud",
   },
   {
@@ -54,29 +54,6 @@ const portfolioItems = [
     image: "/jamselinas.png",
     link: "https://jamselinas13.ngalamfoldingbike.id/",
     alt: "jamselinas",
-  },
-  {
-    title: "SAFD Absence Generator",
-    description: "Generator Absence BBCode for San Andreas Fire Department.",
-    image: "/generator.png",
-    link: "https://safd-absence-generator.vercel.app/",
-    alt: "generator",
-  },
-  {
-    title: "SAFD Absence Finder",
-    description:
-      "Absence finder by district or name for San Andreas Fire Department",
-    image: "/finder.png",
-    link: "https://safd-absence-finder.vercel.app/",
-    alt: "finder",
-  },
-  {
-    title: "Component Calculator",
-    description:
-      "Calculator component for mechanic Jogjagmers Reality Project. Built with simple HTML, Bootstrap, and JavaScript.",
-    image: "/component.png",
-    link: "https://component-calculator.vercel.app/",
-    alt: "component",
   },
 ];
 export default portfolioItems;
